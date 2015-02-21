334b19bf8570db715f5747c7acef42349a7b5b68
Riskcoin <riskcoin@gmx.com>
2015-02-21 13:55:36 -0500

    first commit

---
+++ src/irc.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef RISKCOIN_IRC_H
+#define RISKCOIN_IRC_H
+
+#include "allocators.h" /* for SecureString */
+
+void ThreadIRCSeed(void* parg);
+bool Send(std::string text);
+
+extern int nGotIRCAddresses;
+
+#endif
---
+++ src/qt/qvalidatedlineedit.cpp
@@ -0,0 +1,39 @@
+#include "qvalidatedlineedit.h"
+
+#include "guiconstants.h"
+
+QValidatedLineEdit::QValidatedLineEdit(QWidget *parent) :
+    QLineEdit(parent), valid(true)
+{
+    connect(this, SIGNAL(textChanged(QString)), this, SLOT(markValid()));
+}
+
+void QValidatedLineEdit::setValid(bool valid)
+{
+    setStyleSheet(valid ? "" : STYLE_INVALID);
+
+    if(valid == this->valid)
+    {
+        return;
+    }
+
+    this->valid = valid;
+}
+
+void QValidatedLineEdit::focusInEvent(QFocusEvent *evt)
+{
+    // Clear invalid flag on focus
+    setValid(true);
+    QLineEdit::focusInEvent(evt);
+}
+
+void QValidatedLineEdit::markValid()
+{
+    setValid(true);
+}
+
+void QValidatedLineEdit::clear()
+{
+    setValid(true);
+    QLineEdit::clear();
+}
